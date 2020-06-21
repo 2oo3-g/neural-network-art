1f69ec441c5bfaeaa6bda8152769d9e4a9273a61
Azmain Adel <azmainadel47@gmail.com>
2020-06-21 20:49:08 +0600

    :zap: Add I/O Methods and basic structure

---
+++ Solution/sequence_generator.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class SequenceGenerator
+{
+private:
+    vector<int> inputSequence;
+    vector<int> outputSequences[5];
+
+    vector<int> getInputSequenceCopy()
+    {
+        vector<int> temp = inputSequence;
+
+        return temp;
+    }
+
+    vector<int> perturbateBySwap()
+    {
+        vector<int> outputSequence = getInputSequenceCopy();
+    }
+
+    vector<int> perturbateByShift()
+    {
+        vector<int> outputSequence = getInputSequenceCopy();
+    }
+
+    void loadInputSequence(char *fileName)
+    {
+        ifstream inFile;
+        int jobId;
+
+        inFile.open(fileName);
+        if (!inFile)
+        {
+            printf("\nFile opening error: %s\n", fileName);
+            exit(0);
+        }
+
+        while (inFile >> jobId)
+        {
+            inputSequence.push_back(jobId);
+        }
+
+        inFile.close();
+    }
+
+    void saveOutputSequences(){
+        ofstream outFile("input/input.dat");
+
+        for(auto i = 0; i < 5; i++){
+            vector<int> currentSequence = outputSequences[i];
+
+            for (auto j = currentSequence.begin(); j != currentSequence.end(); j++) {
+                outFile << *j << " ";
+            }
+
+            outFile << endl;
+        }
+    }
+
+public:
+    SequenceGenerator();
+
+
+    void runEngine(char *fileName)
+    {
+        loadInputSequence(fileName);
+        saveOutputSequences();
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    SequenceGenerator sequenceGenerator = SequenceGenerator();
+
+    if (argc > 1)
+    {
+        sequenceGenerator.runEngine(argv[1]);
+    }
+    else
+    {
+        printf("Enter job sequence file name\n");
+        exit(0);
+    }
+
+    return 0;
+}
